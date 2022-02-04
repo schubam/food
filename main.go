0f623ba4193dada9072ae76f32d0b95109a197be
Marc Schuba <me@schubam.com>
2022-02-04 09:30:10 +0100

    feat: sqlite repository

--- main.go
+++ main.go
@@ -1,7 +1,87 @@
 package main
 
-import "fmt"
+import (
+	"database/sql"
+	"fmt"
+	_ "github.com/mattn/go-sqlite3"
+	"github.com/schubam/food/meal_item"
+	"log"
+	"os"
+)
+
+const fileName = "sqlite.db"
 
 func main() {
-	fmt.Println("Hello World")
+	os.Remove(fileName)
+
+	db, err := sql.Open("sqlite3", fileName)
+	if err != nil {
+		log.Fatalf("error opening file: %v\n", err)
+	}
+
+	mealItemsRepository := mealitem.NewSQLiteRepository(db)
+	if err := mealItemsRepository.Migrate(); err != nil {
+		log.Fatalf("error migrating db: %v\n", err)
+	}
+
+	tunasalad := mealitem.MealItem{
+		Name:     "Tuna Salad",
+		Calories: 420,
+		Carbs:    123,
+		Fat:      12,
+		Protein:  69,
+	}
+
+	broccolistew := mealitem.MealItem{
+		Name:     "Broccoli Casserole",
+		Calories: 469,
+		Carbs:    312,
+		Fat:      21,
+		Protein:  96,
+	}
+
+	createdTunasalad, err := mealItemsRepository.Create(tunasalad)
+	if err != nil {
+		log.Fatalf("error Create(): %v\n", err)
+	}
+
+	createdBroccoli, err := mealItemsRepository.Create(broccolistew)
+	if err != nil {
+		log.Fatalf("error Create(): %v\n", err)
+	}
+
+	gotTunasalad, err := mealItemsRepository.GetByName("Tuna Salad")
+	if err != nil {
+		log.Fatalf("error GetByName(): %v\n", err)
+	}
+
+	fmt.Printf("get by name: %+v\n", gotTunasalad)
+
+	createdTunasalad.Calories = 666
+	if _, err := mealItemsRepository.Update(createdTunasalad.ID, *createdTunasalad); err != nil {
+		log.Fatalf("error Update(): %v\n", err)
+	}
+
+	all, err := mealItemsRepository.All()
+	if err != nil {
+		log.Fatalf("error All(): %v\n", err)
+	}
+
+	fmt.Printf("\nAll mealitems:\n")
+	for _, mealitem := range all {
+		fmt.Printf("mealitem: %+v\n", mealitem)
+	}
+
+	if err := mealItemsRepository.Delete(createdBroccoli.ID); err != nil {
+		log.Fatalf("error Delete(): %v\n", err)
+	}
+
+	all, err = mealItemsRepository.All()
+	if err != nil {
+		log.Fatalf("error All(): %v\n", err)
+	}
+	fmt.Printf("\nAll mealitems:\n")
+	for _, mealitem := range all {
+		fmt.Printf("mealitem: %+v\n", mealitem)
+	}
 }
--- meal_item/sqlite_repository.go
+++ meal_item/sqlite_repository.go
@@ -38,7 +38,7 @@ func (r *SQLiteRepository) Migrate() error {
         calories INTEGER NOT NULL,
         carbs INTEGER NOT NULL,
         fat INTEGER NOT NULL,
-        protein INTEGER NOT NULL,
+        protein INTEGER NOT NULL
     );
     `
 
@@ -66,4 +66,77 @@ func (r *SQLiteRepository) Create(mealitem MealItem) (*MealItem, error) {
 	mealitem.ID = id
 
 	return &mealitem, nil
+}
+
+func (r *SQLiteRepository) All() ([]MealItem, error) {
+	rows, err := r.db.Query("SELECT * FROM mealitems")
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	var all []MealItem
+	for rows.Next() {
+		var item MealItem
+		if err := rows.Scan(&item.ID, &item.Name, &item.Calories, &item.Carbs,
+			&item.Fat, &item.Protein); err != nil {
+			return nil, err
+		}
+		all = append(all, item)
+	}
+
+	return all, nil
+}
+
+func (r *SQLiteRepository) GetByName(name string) (*MealItem, error) {
+	row := r.db.QueryRow("SELECT * FROM mealitems WHERE name = ?", name)
+
+	var item MealItem
+	if err := row.Scan(&item.ID, &item.Name, &item.Calories, &item.Carbs,
+		&item.Fat, &item.Protein); err != nil {
+		return nil, ErrNotExists
+	}
+	return &item, nil
+}
+
+func (r *SQLiteRepository) Update(id int64, updated MealItem) (*MealItem, error) {
+	if id == 0 {
+		return nil, errors.New("invalid updated ID")
+	}
+
+	res, err := r.db.Exec(`UPDATE mealitems SET name = ?, calories = ?, carbs =
+    ?, fat = ?, protein = ? WHERE id = ?`, updated.Name, updated.Calories,
+		updated.Carbs, updated.Fat, updated.Protein, id)
+	if err != nil {
+		return nil, err
+	}
+
+	rowsAffected, err := res.RowsAffected()
+	if err != nil {
+		return nil, err
+	}
+
+	if rowsAffected == 0 {
+		return nil, ErrUpdateFailed
+	}
+
+	return &updated, nil
+}
+
+func (r *SQLiteRepository) Delete(id int64) error {
+	res, err := r.db.Exec(`DELETE FROM mealitems WHERE id = ?`, id)
+	if err != nil {
+		return err
+	}
+
+	rowsAffected, err := res.RowsAffected()
+	if err != nil {
+		return err
+	}
+
+	if rowsAffected == 0 {
+		return ErrDeleteFailed
+	}
+
+	return nil
 }
